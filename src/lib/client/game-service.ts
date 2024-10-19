77d3d9b928a5f13038aad40f68f6991234597071
rmadhuram <rmadhuram@yahoo.com>
2024-10-18 20:56:10 -0700

    time taken calculations

--- src/lib/client/game-service.ts
+++ src/lib/client/game-service.ts
@@ -14,9 +14,25 @@ export function startGame(name: string, type: "kid" | "adult") {
     gameLevel: 1,
     maxCompletedLevel: 0,
     totalTime: 0,
+    totalHighResTime: 0,
   };
 
   currentGame = game;
+}
+
+/**
+ * Clone the current game and promote to the next level
+ */
+export function promoteNextLevel(timeTaken: number, highResTime: number): Game {
+  if (!currentGame) {
+    throw new Error("No game started");
+  }
+  const game = JSON.parse(JSON.stringify(currentGame));
+  game.gameLevel++;
+  game.totalTime += timeTaken;
+  game.totalHighResTime += highResTime;
+  currentGame = game;
+  return game;
 }
 
 export function getGame() {
